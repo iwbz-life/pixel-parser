8694d0ebbf513aec1d8806e50f3a6fc80fd0805d
iwbz-life <interwebs.life@gmail.com>
2020-05-03 22:30:59 -0400

    Wrap referrer in quotes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const lineRx = /(.*)\s+\/interwebs.gif\?life=(.*?)\s+(.+?)\s+(.+?)\s+(.+?)\s+"ht
 rl.on('line', (line) => {
   if (lineRx.test(line)) {
     console.log(line.replace(lineRx, (match, p1, p2, p3, p4, p5, p6, p7) => {
-      return [ p1, p6, p3, p4, p5, decodeURIComponent(p2), p7].join(' ');
+      return [ p1, p6, p3, p4, p5, `"${decodeURIComponent(p2)}"`, p7].join(' ');
     }));
   }
 })
